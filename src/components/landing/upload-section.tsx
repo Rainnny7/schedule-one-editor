b2c581aaca9ac0cf9f6e93a8217a721d2ecd9f6f
Rainnny7 <braydonrainnny@gmail.com>
2025-04-07 18:42:39 -0400

    simple dropzone design

--- src/components/landing/upload-section.tsx
+++ src/components/landing/upload-section.tsx
@@ -1,14 +1,23 @@
 "use client";
 
+import { Upload } from "lucide-react";
 import { ReactElement } from "react";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "~/components/ui/dialog";
 import { cn } from "~/lib/utils";
 
 const UploadSection = (): ReactElement => {
     return (
         <section
             id="upload"
-            className="relative flex h-[50rem] w-full items-center justify-center bg-background"
+            className="relative flex h-[40rem] w-full items-center justify-center bg-background"
         >
+            {/* Background */}
             <div
                 className={cn(
                     "absolute inset-0",
@@ -19,8 +28,35 @@ const UploadSection = (): ReactElement => {
             <div className="absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)] bg-background pointer-events-none" />
 
             {/* Content */}
-            <div className="relative z-20">Upload</div>
+            <div className="relative z-20">
+                <Dropzone />
+            </div>
         </section>
     );
 };
+
+const Dropzone = (): ReactElement => {
+    return (
+        <div className="p-5 w-[50rem] h-64 flex flex-col gap-2.5 justify-center items-center bg-card/60 text-sm text-muted-foreground border border-border rounded-lg">
+            <Upload className="size-6" />
+            <span>Click to upload or drag and drop</span>
+            <GetSaveTutorial />
+        </div>
+    );
+};
+
+const GetSaveTutorial = (): ReactElement => (
+    <Dialog>
+        <DialogTrigger className="px-2 py-1 text-xs font-light rounded-lg bg-muted-foreground/10 hover:opacity-75 transition-opacity transform-gpu cursor-pointer">
+            How do I get my Schedule I save?
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>How do I get my Schedule I save?</DialogTitle>
+            </DialogHeader>
+            Hello World
+        </DialogContent>
+    </Dialog>
+);
+
 export default UploadSection;
